b01e19ce80610738674d4fa78da2b50d94543029
Saim Mirza <saim@shadowriser.com>
2026-06-28 10:31:46 +0500

    feat: deploy static site

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,16 @@
-     1|import type { NextConfig } from "next";
-     2|
-     3|const nextConfig: NextConfig = {
-     4|  output: "export",
-     5|  /* config options here */
-     6|  typescript: {
-     7|    ignoreBuildErrors: true,
-     8|  },
-     9|  reactStrictMode: false,
-    10|  allowedDevOrigins: ['*.space-z.ai'],
-    11|  basePath: "/real-estate-lead-capture",
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  reactStrictMode: false,
+  allowedDevOrigins: ['*.space-z.ai'],
+  basePath: "/real-estate-lead-capture",
   assetPrefix: "/real-estate-lead-capture/",
   images: { unoptimized: true },
 };
-    12|
-    13|export default nextConfig;
-    14|
+
+export default nextConfig;
